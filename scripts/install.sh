fb7355c7d31fa1aaf90f83b958f8d10f8cd8d441
Tucker Sullivan <gibsonguy911@gmail.com>
2024-09-07 18:57:39 -0500

    Update install script and stow local ignore

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,14 @@ cd ~ && \
 curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && \
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
 nvm install --lts && npm i -g npm && \
-curl -o https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash && \
+wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash && \
 curl -o https://ohmyposh.dev/install.sh | bash && \
 curl -O https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && \
 chmod u+x nvim.appimage && ./nvim.appimage && \
 git clone https://github.com/Gibsonguy911/stow.git ~/dotfiles && \
-cd ~/dotfiles && stow . && \
+rm ~/.zshrc && cd ~/dotfiles && stow . && \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 cd ~/.fzf && ./install --key-bindings --completion --no-update-rc
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab && \
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
