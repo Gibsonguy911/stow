7b24a9932e3d171c06caf9b7afcfdba650e27e1b
Tucker Sullivan <gibsonguy911@gmail.com>
2024-09-07 03:01:22 -0500

    Fix install script up to nvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
-apt-get -y install $(< packages.list) && \
+sudo apt-get -y install $(< packages.list) && \
+cd ~ && \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && \
+export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
 nvm install --lts && npm i -g npm && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
 curl -s https://ohmyposh.dev/install.sh | bash -s && \
@@ -8,5 +10,4 @@ chmod u+x nvim.appimage && ./nvim.appimage && \
 git clone https://github.com/Gibsonguy911/stow.git ~/dotfiles && \
 cd ~/dotfiles && stow . && \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
-cd ~/.fzf && ./install --key-bindings --completion --no-update-rc && \
-
+cd ~/.fzf && ./install --key-bindings --completion --no-update-rc
