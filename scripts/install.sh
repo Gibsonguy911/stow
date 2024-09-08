f6461834e5da5ecd153ae6ec884ab1f2273667c0
Tucker Sullivan <gibsonguy911@gmail.com>
2024-09-07 19:06:28 -0500

    Update stow repo to clone by ssh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 sudo apt-get -y install $(< packages.list) && \
 cd ~ && \
+git clone git@github.com:Gibsonguy911/stow.git ~/dotfiles && \
 curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && \
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
 nvm install --lts && npm i -g npm && \
@@ -7,7 +8,6 @@ wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.
 curl -o https://ohmyposh.dev/install.sh | bash && \
 curl -O https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && \
 chmod u+x nvim.appimage && ./nvim.appimage && \
-git clone https://github.com/Gibsonguy911/stow.git ~/dotfiles && \
 rm ~/.zshrc && cd ~/dotfiles && stow . && \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 cd ~/.fzf && ./install --key-bindings --completion --no-update-rc
