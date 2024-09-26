7e583f0f8221eb5779ae79d3879e87dcace76486
Tucker Sullivan <tuckersullivan@ymail.com>
2024-09-26 11:19:12 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash &
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
 nvm install --lts && npm i -g npm && \
 wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash && \
-curl -o https://ohmyposh.dev/install.sh | bash && \
+curl -o- https://ohmyposh.dev/install.sh | bash && \
 rm ~/.zshrc && cd ~/dotfiles && stow . && \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 cd ~/.fzf && ./install --key-bindings --completion --no-update-rc
